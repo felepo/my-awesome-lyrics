afab993075382771858a72bed735f2848c5f9e9f
felepo <fecrackle@gmail.com>
2025-05-15 12:41:34 -0600

    Creating New Song Form

---
+++ app/dashboard/songs/create/page.tsx
@@ -0,0 +1,10 @@
+import Form from '@/app/ui/songs/create-form';
+
+export default function Page() {
+  return (
+    <main>
+      <h1>Crear Nueva Canción</h1>
+      <Form />
+    </main>
+  );
+}
--- app/dashboard/songs/page.tsx
+++ app/dashboard/songs/page.tsx
@@ -1,9 +1,13 @@
 import SongsTable from "@/app/ui/songs/table";
+import { CreateSong } from '@/app/ui/songs/buttons';
 
 export default function Page () {
   return (
     <div className="w-full">
       <p>Songs</p>
+      <div className="m-t4 flex items-center justify-between gap-2 md:mt-8">
+        <CreateSong />
+      </div>
       <SongsTable />
     </div>
   );
