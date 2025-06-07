82928c58d14dc6574bc3a093cd07b47d65c81217
felepo <fecrackle@gmail.com>
2025-06-06 23:48:02 -0600

    Adding styles to songs list page

--- app/dashboard/songs/page.tsx
+++ app/dashboard/songs/page.tsx
@@ -1,14 +1,19 @@
 import SongsTable from "@/app/ui/songs/table";
 import { CreateSong } from '@/app/ui/songs/buttons';
+import { lusitana } from '@/app/ui/fonts';
 
 export default function Page () {
   return (
     <div className="w-full">
-      <p>Songs</p>
+      <div className='flex w-full items-center justify-between'>
+        <h1 className={`${lusitana.className} text-2x1`}>Songs</h1>
+      </div>
       <div className="m-t4 flex items-center justify-between gap-2 md:mt-8">
         <CreateSong />
       </div>
-      <SongsTable />
+      <span>
+        <SongsTable />
+      </span>
     </div>
   );
 }
--- app/ui/songs/table.tsx
+++ app/ui/songs/table.tsx
@@ -5,16 +5,44 @@ export default async function SongsTable() {
   console.log(songs);
 
   return (
-    <div>
-      {songs?.map((song) => (
-        <div 
-          key={song.id}
-          className='py-6'
-        >
-          <div>{song.name}</div>
-          <div>{song.author}</div>
+    <div className="mt-6 flow-root">
+      <div className="inline-block min-w-full align-middle">
+        <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
+          <div className="md:hidden">
+            {songs?.map((song) => (
+              <div
+                key={song.id}
+                className="mb-2 w-full rounded-md bg-white p-4"
+              >
+                <div className="flex items-center justify-between border-b pb-4">
+                  <div>
+                    <div className="mb-2 flex items-center">
+                      <p>{song.name}</p>
+                    </div>
+                    <p>{song.author}</p>
+                  </div>
+                </div>
+                <div className="flex w-full items-center justify-between pt-4">
+                  <p>Lyrics</p>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
-      ))}
+      </div>
     </div>
+
+
+    // <div>
+    //   {songs?.map((song) => (
+    //     <div 
+    //       key={song.id}
+    //       className='py-6'
+    //     >
+    //       <div>{song.name}</div>
+    //       <div>{song.author}</div>
+    //     </div>
+    //   ))}
+    // </div>
   );
 }
