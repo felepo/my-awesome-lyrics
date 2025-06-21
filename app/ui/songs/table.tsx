85c5706fc627bd79efab44dd838ff7f7bb08e3a2
felepo <fecrackle@gmail.com>
2025-06-21 00:30:27 -0600

    Adding song table on the dashboard

--- app/ui/songs/table.tsx
+++ app/ui/songs/table.tsx
@@ -14,6 +14,7 @@ export default async function SongsTable({
     <div className="mt-6 flow-root">
       <div className="inline-block min-w-full align-middle">
         <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
+          {/* Mobile list */}
           <div className="md:hidden">
             {songs?.map((song) => (
               <div
@@ -38,6 +39,49 @@ export default async function SongsTable({
               </div>
             ))}
           </div>
+          {/* Desktop table */}
+          <table className="hidden min-w-full text-gray-900 md:table">
+            <thead className="rounded-lg text-left text-sm font-normal">
+              <tr>
+                <th scope="col" className="px-4 py-5 font-medium sm:pl-6">
+                  Nombre
+                </th>
+                <th scope="col" className="px-3 py-5 font-medium">
+                  Autor
+                </th>
+                <th scope="col" className="p-3 py-5 font-medium">
+                  Letras
+                </th>
+                <th scope="col" className="relative py-3 pl-6 pr-3">
+                  <span className="sr-only">Edición</span>
+                </th>
+              </tr>
+            </thead>
+            <tbody className="bg-white">
+              {songs?.map((song) => (
+                <tr
+                  key={song.id}
+                  className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg"
+                >
+                  <td className="whitespace-nowrap px-3 py-3">
+                    {song.name}
+                  </td>
+                  <td className="whitespace-nowrap px-3 py-3">
+                    {song.author}
+                  </td>
+                  <td className="whitespace-nowrap px-3 py-3">
+                    <p className="text-xs italic text-gray-700">{song.lyrics}</p>
+                  </td>
+                  <td className="whitespace-nowrap py-3 pl-6 pr-3">
+                    <div className="flex justify-end gap-3">
+                      <UpdateSong id={song.id} />
+                      <DeleteSong id={song.id} />
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
