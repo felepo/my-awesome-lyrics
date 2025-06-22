8bc7b60ed20486339844cf10853df27da56ca028
felepo <fecrackle@gmail.com>
2025-06-22 00:56:38 -0600

    Adjusting styles for the dashboard sidenav

--- app/ui/main-logo.tsx
+++ app/ui/main-logo.tsx
@@ -1,4 +1,4 @@
-import { FireIcon } from '@heroicons/react/24/outline';
+import { AdjustmentsHorizontalIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
 
 export default function MainLogo() {
@@ -6,8 +6,8 @@ export default function MainLogo() {
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
-      <FireIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">ILPV</p>
+      <AdjustmentsHorizontalIcon className="h-12 w-12 flex-shrink-0" />
+      <p className="text-[44px] ml-4 flex-shrink-0">ILPV</p>
     </div>
   );
 }
